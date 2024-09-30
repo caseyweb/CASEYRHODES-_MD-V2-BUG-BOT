15e62b4a334560e5a4723fee3a5263964edc64e1
caseyweb <caseywebsthedonn25@gmail.com>
2024-09-30 04:34:16 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+//base by DGXeon
+//re-upload? recode? copy code? give credit ya :)
+//YouTube: @GlobalTechInfo
+//Telegram: https://t.me/GlobalTechInc
+//GitHub: @GlobalTechInfo
+//WhatsApp: https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@GlobalTechInfo
+
 require('./settings')
 const pino = require('pino')
 const { Boom } = require('@hapi/boom')
@@ -9,7 +17,7 @@ const axios = require('axios')
 const PhoneNumber = require('awesome-phonenumber')
 const { imageToWebp, videoToWebp, writeExifImg, writeExifVid } = require('./lib/exif')
 const { smsg, isUrl, generateMessageTag, getBuffer, getSizeMedia, fetch, await, sleep, reSize } = require('./lib/myfunc')
-const { default: XeonBotIncConnect, delay, PHONENUMBER_MCC, makeCacheableSignalKeyStore, useMultiFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto, Browsers} = require("@whiskeysockets/baileys")
+const { default: GlobalTechIncConnect, delay, PHONENUMBER_MCC, makeCacheableSignalKeyStore, useMultiFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto, Browsers} = require("@whiskeysockets/baileys")
 const NodeCache = require("node-cache")
 const Pino = require("pino")
 const readline = require("readline")
@@ -32,12 +40,12 @@ const useMobile = process.argv.includes("--mobile")
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
 const question = (text) => new Promise((resolve) => rl.question(text, resolve))
          
-async function startXeonBotInc() {
+async function startGlobalTechInc() {
 //------------------------------------------------------
 let { version, isLatest } = await fetchLatestBaileysVersion()
 const {  state, saveCreds } =await useMultiFileAuthState(`./session`)
     const msgRetryCounterCache = new NodeCache() // for retry message, "waiting message"
-    const XeonBotInc = makeWASocket({
+    const GlobalTechInc = makeWASocket({
         logger: pino({ level: 'silent' }),
         printQRInTerminal: !pairingCode, // popping up QR in terminal log
       browser: Browsers.windows('Firefox'), // for this issues https://github.com/WhiskeySockets/Baileys/issues/328
@@ -57,11 +65,11 @@ const {  state, saveCreds } =await useMultiFileAuthState(`./session`)
       defaultQueryTimeoutMs: undefined, // for this issues https://github.com/WhiskeySockets/Baileys/issues/276
    })
    
-   store.bind(XeonBotInc.ev)
+   store.bind(GlobalTechInc.ev)
 
     // login use pairing code
    // source code https://github.com/WhiskeySockets/Baileys/blob/master/Example/example.ts#L61
-   if (pairingCode && !XeonBotInc.authState.creds.registered) {
+   if (pairingCode && !GlobalTechInc.authState.creds.registered) {
       if (useMobile) throw new Error('Cannot use pairing code with mobile api')
 
       let phoneNumber
@@ -69,7 +77,7 @@ const {  state, saveCreds } =await useMultiFileAuthState(`./session`)
          phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
 
          if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
-            console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +254112192119 `)))
+            console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +254112192119")))
             process.exit(0)
          }
       } else {
@@ -87,39 +95,39 @@ const {  state, saveCreds } =await useMultiFileAuthState(`./session`)
       }
 
       setTimeout(async () => {
-         let code = await XeonBotInc.requestPairingCode(phoneNumber)
+         let code = await GlobalTechInc.requestPairingCode(phoneNumber)
          code = code?.match(/.{1,4}/g)?.join("-") || code
          console.log(chalk.black(chalk.bgGreen(`Your Pairing Code : `)), chalk.black(chalk.white(code)))
       }, 3000)
    }
 
-    XeonBotInc.ev.on('messages.upsert', async chatUpdate => {
+    GlobalTechInc.ev.on('messages.upsert', async chatUpdate => {
         //console.log(JSON.stringify(chatUpdate, undefined, 2))
         try {
             const mek = chatUpdate.messages[0]
             if (!mek.message) return
             mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
             if (mek.key && mek.key.remoteJid === 'status@broadcast' )
-            if (!XeonBotInc.public && !mek.key.fromMe && chatUpdate.type === 'notify') return
+            if (!GlobalTechInc.public && !mek.key.fromMe && chatUpdate.type === 'notify') return
             if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16) return
-            const m = smsg(XeonBotInc, mek, store)
-            require("./XeonBug8")(XeonBotInc, m, chatUpdate, store)
+            const m = smsg(GlobalTechInc, mek, store)
+            require("./XeonBug8")(GlobalTechInc, m, chatUpdate, store)
         } catch (err) {
             console.log(err)
         }
     })
     
     //autostatus view
-        XeonBotInc.ev.on('messages.upsert', async chatUpdate => {
+        GlobalTechInc.ev.on('messages.upsert', async chatUpdate => {
         	if (global.autoswview){
             mek = chatUpdate.messages[0]
             if (mek.key && mek.key.remoteJid === 'status@broadcast') {
-            	await XeonBotInc.readMessages([mek.key]) }
+            	await GlobalTechInc.readMessages([mek.key]) }
             }
     })
 
    
-    XeonBotInc.decodeJid = (jid) => {
+    GlobalTechInc.decodeJid = (jid) => {
         if (!jid) return jid
         if (/:\d+@/gi.test(jid)) {
             let decode = jidDecode(jid) || {}
@@ -127,9 +135,9 @@ const {  state, saveCreds } =await useMultiFileAuthState(`./session`)
         } else return jid
     }
 
-    XeonBotInc.ev.on('contacts.update', update => {
+    GlobalTechInc.ev.on('contacts.update', update => {
         for (let contact of update) {
-            let id = XeonBotInc.decodeJid(contact.id)
+            let id = GlobalTechInc.decodeJid(contact.id)
             if (store && store.contacts) store.contacts[id] = {
                 id,
                 name: contact.notify
@@ -137,39 +145,38 @@ const {  state, saveCreds } =await useMultiFileAuthState(`./session`)
         }
     })
 
-    XeonBotInc.getName = (jid, withoutContact = false) => {
-        id = XeonBotInc.decodeJid(jid)
-        withoutContact = XeonBotInc.withoutContact || withoutContact
+    GlobalTechInc.getName = (jid, withoutContact = false) => {
+        id = GlobalTechInc.decodeJid(jid)
+        withoutContact = GlobalTechInc.withoutContact || withoutContact
         let v
         if (id.endsWith("@g.us")) return new Promise(async (resolve) => {
             v = store.contacts[id] || {}
-            if (!(v.name || v.subject)) v = XeonBotInc.groupMetadata(id) || {}
+            if (!(v.name || v.subject)) v = GlobalTechInc.groupMetadata(id) || {}
             resolve(v.name || v.subject || PhoneNumber('+' + id.replace('@s.whatsapp.net', '')).getNumber('international'))
         })
         else v = id === '0@s.whatsapp.net' ? {
                 id,
                 name: 'WhatsApp'
-            } : id === XeonBotInc.decodeJid(XeonBotInc.user.id) ?
-            XeonBotInc.user :
+            } : id === GlobalTechInc.decodeJid(GlobalTechInc.user.id) ?
+            GlobalTechInc.user :
             (store.contacts[id] || {})
         return (withoutContact ? '' : v.name) || v.subject || v.verifiedName || PhoneNumber('+' + jid.replace('@s.whatsapp.net', '')).getNumber('international')
     }
     
-    XeonBotInc.public = true
+    GlobalTechInc.public = true
 
-    XeonBotInc.serializeM = (m) => smsg(XeonBotInc, m, store)
+    GlobalTechInc.serializeM = (m) => smsg(GlobalTechInc, m, store)
 
-XeonBotInc.ev.on("connection.update",async  (s) => {
+GlobalTechInc.ev.on("connection.update",async  (s) => {
         const { connection, lastDisconnect } = s
         if (connection == "open") {
         	console.log(chalk.magenta(` `))
-            console.log(chalk.yellow(`🌿Connected to => ` + JSON.stringify(XeonBotInc.user, null, 2)))
+            console.log(chalk.yellow(`🌿Connected to => ` + JSON.stringify(GlobalTechInc.user, null, 2)))
 			await delay(1999)
             console.log(chalk.yellow(`\n\n                  ${chalk.bold.blue(`[ ${botname} ]`)}\n\n`))
             console.log(chalk.cyan(`< ================================================== >`))
-	        console.log(chalk.magenta(`\n${themeemoji} YT CHANNEL: Caseyrhodes`))
-            console.log(chalk.magenta(`${themeemoji} GITHUB: CASEYRHODES-_MD-V2-BUG-BOT`))
-            console.log(chalk.magenta(`${themeemoji} INSTAGRAM: @caseywebs`))
+	        console.log(chalk.magenta(`\n${themeemoji} YT CHANNEL: Caseyrhodes001`))
+            console.log(chalk.magenta(`${themeemoji} GITHUB: Caseywebs`))
             console.log(chalk.magenta(`${themeemoji} WA NUMBER: ${owner}`))
             console.log(chalk.magenta(`${themeemoji} CREDIT: ${wm}\n`))
         }
@@ -179,27 +186,27 @@ XeonBotInc.ev.on("connection.update",async  (s) => {
             lastDisconnect.error &&
             lastDisconnect.error.output.statusCode != 401
         ) {
-            startXeonBotInc()
+            startGlobalTechInc()
         }
     })
-    XeonBotInc.ev.on('creds.update', saveCreds)
-    XeonBotInc.ev.on("messages.upsert",  () => { })
+    GlobalTechInc.ev.on('creds.update', saveCreds)
+    GlobalTechInc.ev.on("messages.upsert",  () => { })
 
-    XeonBotInc.sendText = (jid, text, quoted = '', options) => XeonBotInc.sendMessage(jid, {
+    GlobalTechInc.sendText = (jid, text, quoted = '', options) => GlobalTechInc.sendMessage(jid, {
         text: text,
         ...options
     }, {
         quoted,
         ...options
     })
-    XeonBotInc.sendTextWithMentions = async (jid, text, quoted, options = {}) => XeonBotInc.sendMessage(jid, {
+    GlobalTechInc.sendTextWithMentions = async (jid, text, quoted, options = {}) => GlobalTechInc.sendMessage(jid, {
         text: text,
         mentions: [...text.matchAll(/@(\d{0,16})/g)].map(v => v[1] + '@s.whatsapp.net'),
         ...options
     }, {
         quoted
     })
-    XeonBotInc.sendImageAsSticker = async (jid, path, quoted, options = {}) => {
+    GlobalTechInc.sendImageAsSticker = async (jid, path, quoted, options = {}) => {
         let buff = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,` [1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
         let buffer
         if (options && (options.packname || options.author)) {
@@ -208,7 +215,7 @@ XeonBotInc.ev.on("connection.update",async  (s) => {
             buffer = await imageToWebp(buff)
         }
 
-        await XeonBotInc.sendMessage(jid, {
+        await GlobalTechInc.sendMessage(jid, {
             sticker: {
                 url: buffer
             },
@@ -218,7 +225,7 @@ XeonBotInc.ev.on("connection.update",async  (s) => {
         })
         return buffer
     }
-    XeonBotInc.sendVideoAsSticker = async (jid, path, quoted, options = {}) => {
+    GlobalTechInc.sendVideoAsSticker = async (jid, path, quoted, options = {}) => {
         let buff = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,` [1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
         let buffer
         if (options && (options.packname || options.author)) {
@@ -227,7 +234,7 @@ XeonBotInc.ev.on("connection.update",async  (s) => {
             buffer = await videoToWebp(buff)
         }
 
-        await XeonBotInc.sendMessage(jid, {
+        await GlobalTechInc.sendMessage(jid, {
             sticker: {
                 url: buffer
             },
@@ -237,7 +244,7 @@ XeonBotInc.ev.on("connection.update",async  (s) => {
         })
         return buffer
     }
-    XeonBotInc.downloadAndSaveMediaMessage = async (message, filename, attachExtension = true) => {
+    GlobalTechInc.downloadAndSaveMediaMessage = async (message, filename, attachExtension = true) => {
         let quoted = message.msg ? message.msg : message
         let mime = (message.msg || message).mimetype || ''
         let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]
@@ -253,7 +260,7 @@ XeonBotInc.ev.on("connection.update",async  (s) => {
         return trueFileName
     }
     
-    XeonBotInc.getFile = async (PATH, save) => {
+    GlobalTechInc.getFile = async (PATH, save) => {
         let res
         let data = Buffer.isBuffer(PATH) ? PATH : /^data:.*?\/.*?;base64,/i.test(PATH) ? Buffer.from(PATH.split`,`[1], 'base64') : /^https?:\/\//.test(PATH) ? await (res = await getBuffer(PATH)) : fs.existsSync(PATH) ? (filename = PATH, fs.readFileSync(PATH)) : typeof PATH === 'string' ? PATH : Buffer.alloc(0)
         //if (!Buffer.isBuffer(data)) throw new TypeError('Result is not a buffer')
@@ -273,8 +280,8 @@ XeonBotInc.ev.on("connection.update",async  (s) => {
 
     }
     
-    XeonBotInc.sendFile = async (jid, path, filename = '', caption = '', quoted, ptt = false, options = {}) => {
-  let type = await XeonBotInc.getFile(path, true);
+    GlobalTechInc.sendFile = async (jid, path, filename = '', caption = '', quoted, ptt = false, options = {}) => {
+  let type = await GlobalTechInc.getFile(path, true);
   let { res, data: file, filename: pathFile } = type;
 
   if (res && res.status !== 200 || file.length <= 65536) {
@@ -321,18 +328,18 @@ XeonBotInc.ev.on("connection.update",async  (s) => {
   let m;
 
   try {
-    m = await XeonBotInc.sendMessage(jid, message, { ...opt, ...options });
+    m = await GlobalTechInc.sendMessage(jid, message, { ...opt, ...options });
   } catch (e) {
     //console.error(e)
     m = null;
   } finally {
-    if (!m) m = await XeonBotInc.sendMessage(jid, { ...message, [mtype]: file }, { ...opt, ...options });
+    if (!m) m = await GlobalTechInc.sendMessage(jid, { ...message, [mtype]: file }, { ...opt, ...options });
     file = null;
     return m;
   }
 }
 
-    XeonBotInc.downloadMediaMessage = async (message) => {
+    GlobalTechInc.downloadMediaMessage = async (message) => {
         let mime = (message.msg || message).mimetype || ''
         let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]
         const stream = await downloadContentFromMessage(message, messageType)
@@ -344,7 +351,7 @@ XeonBotInc.ev.on("connection.update",async  (s) => {
         return buffer
     }
     }
-return startXeonBotInc()
+return startGlobalTechInc()
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
